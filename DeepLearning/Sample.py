56dfa38c466a66cc8664b8069a3a75d9368f242a
Максим Сидоров <makskekmath@gmail.com>
2023-09-21 19:12:09 +0300

    Continued

--- DeepLearning/Sample.py
+++ DeepLearning/Sample.py
@@ -20,9 +20,7 @@ def neural_network(input, weights):
 ntoe = [8.5, 9.5, 9.9, 9]
 nwins = [0.65, 0.8, 0.8, 0.9]
 nfans = [1.2, 1.3, 0.5, 1]
-
-i = 0
-
-input =np.array([ntoe[i], nwins[i], nfans[i]])
-result = neural_network(input, weights)
-print(result)
+for i in range(len(ntoe)):
+    input =np.array([ntoe[i], nwins[i], nfans[i]])
+    result = neural_network(input, weights)
+    print(result)
